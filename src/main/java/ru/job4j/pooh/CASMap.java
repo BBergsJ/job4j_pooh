9f0bfe1ffba875fa905f0728127d620bbc3b890a
BBergsJ <goldlike888@gmail.com>
2021-07-04 22:30:01 +0300

    Тестовое задание - проект "Pooh JMS"

--- src/main/java/ru/job4j/pooh/CASMap.java
+++ src/main/java/ru/job4j/pooh/CASMap.java
@@ -5,16 +5,16 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class CASMap {
     public static void main(String[] args) {
-        ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
-        String name = "weather";
-
-        /* add if empty */
-        queue.putIfAbsent(name, new ConcurrentLinkedQueue<>());
-
-        /* put */
-        queue.get(name).add("value");
-
-        /* extract */
-        var text = queue.get(name, emptyQueue()).poll();
+//        ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
+//        String name = "weather";
+//
+//        /* add if empty */
+//        queue.putIfAbsent(name, new ConcurrentLinkedQueue<>());
+//
+//        /* put */
+//        queue.get(name).add("value");
+//
+//        /* extract */
+//        var text = queue.get(name, emptyQueue()).poll();
     }
 }

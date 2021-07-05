b2629063eb096858772cdc4d2a8ec0db7ca3d626
BBergsJ <goldlike888@gmail.com>
2021-07-05 23:55:19 +0300

    Тестовое задание - проект "Pooh JMS"

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -1,8 +1,14 @@
 package ru.job4j.pooh;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class QueueService implements Service {
+    ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
+
     @Override
     public Resp process(Req req) {
+
         return null;
     }
 }

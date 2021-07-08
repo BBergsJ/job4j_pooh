0fc597994eb48b3dfc25f7087ea061b1f9c327f6
BBergsJ <goldlike888@gmail.com>
2021-07-08 23:25:04 +0300

    Тестовое задание - проект "Pooh JMS"

--- src/main/java/ru/job4j/pooh/TopicService.java
+++ src/main/java/ru/job4j/pooh/TopicService.java
@@ -1,24 +1,19 @@
 package ru.job4j.pooh;
 
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class TopicService implements Service {
-    private final static ConcurrentHashMap<
-            String,
-            ConcurrentHashMap<String, ConcurrentLinkedQueue<String>
-                    >> TOPIC = new ConcurrentHashMap<>();
-    private static int connectedUserId = 1;
+    private final static ConcurrentHashMap<String,
+            ConcurrentHashMap<String, ConcurrentLinkedQueue<String>>> TOPIC = new ConcurrentHashMap<>();
 
     @Override
     public Resp process(Req req) {
         if (req.method().equalsIgnoreCase("post")) {
             TOPIC.putIfAbsent(req.queueName(), new ConcurrentHashMap<>() {{
-                put(String.valueOf(connectedUserId), new ConcurrentLinkedQueue<>());
+                put(req.userId(), new ConcurrentLinkedQueue<>());
             }});
-            TOPIC.get(req.queueName()).get(String.valueOf(connectedUserId)).add(req.text());
-            connectedUserId++;
+            TOPIC.get(req.queueName()).get(req.userId()).add(req.text());
             return new Resp("Posted " + req.queueName(), 200);
         } else if (req.method().equalsIgnoreCase("get")) {
             return TOPIC.containsKey(req.queueName())
